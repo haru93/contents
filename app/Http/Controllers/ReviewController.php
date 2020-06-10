46476a692994bb2661b04cc4179bf703d74255bc
haru93 <syuesuta@yahoo.co.jp>
2020-06-10 11:38:53 +0900

    14 commit

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -21,9 +21,6 @@ class ReviewController extends Controller
             //空白で区切る
             $search_split2 = preg_split('/[\s]+/', $search_split,-1,PREG_SPLIT_NO_EMPTY);
             //単語をループで回す
-            // foreach($search_split2 as $value) {
-            //     $query->where('title','like','%'.$value.'%');
-            // }
             foreach($search_split2 as $value) {
                 $query->where('title','like','%'.$value.'%')
                       ->orwhere('body','like','%'.$value.'%');
--- resources/views/review/index.blade.php
+++ resources/views/review/index.blade.php
@@ -5,24 +5,36 @@
 @endsection
 
 @section('content')
-  <div class="row justify-content-center">
-    <!-- レビューの数だけパネルを表示 -->
-    @foreach($reviews as $review)
-      <div class="col-md-4">
-          <div class="card mb50">
-              <div class="card-body">
-                  @if(!empty($review->image))
-                    <div class='image-wrapper'><img class='content-image' src="{{ asset('storage/images/'.$review->image) }}"></div>
-                  @else
-                    <div class='image-wrapper'><img class='content-image' src="{{ asset('images/dummy.png') }}"></div>
-                  @endif
-                  <h3 class='h3 content-title'>{{ $review->title }}</h3>
-                  <p class='description'>{{ $review->body }}</p>
-                  <a href="{{ route('review.show', ['id' => $review->id ]) }}" class='btn btn-secondary detail-btn'>詳細</a>
-              </div>
-          </div>
+  <div class="container">
+    <!-- 検索バー -->
+    <div class="row justify-content-end pr-3 pb-4">
+      <div class="col-mb-4">
+        <form method="GET" action="{{ route('review.index') }}" class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" name="search" type="search" placeholder="検索" aria-label="Search">
+          <button class="btn btn-primary my-2 my-sm-0" type="submit">検索する</button>
+        </form>
       </div>
-    @endforeach
+    </div>
+
+    <div class="row justify-content-center">
+      <!-- レビューの数だけパネルを表示 -->
+      @foreach($reviews as $review)
+        <div class="col-md-4">
+            <div class="card mb50">
+                <div class="card-body">
+                    @if(!empty($review->image))
+                      <div class='image-wrapper'><img class='content-image' src="{{ asset('storage/images/'.$review->image) }}"></div>
+                    @else
+                      <div class='image-wrapper'><img class='content-image' src="{{ asset('images/dummy.png') }}"></div>
+                    @endif
+                    <h3 class='h3 content-title'>{{ $review->title }}</h3>
+                    <p class='description'>{{ $review->body }}</p>
+                    <a href="{{ route('review.show', ['id' => $review->id ]) }}" class='btn btn-secondary detail-btn'>詳細</a>
+                </div>
+            </div>
+        </div>
+      @endforeach
+    </div>
+    {{ $reviews->links() }}
   </div>
-  {{ $reviews->links() }}
 @endsection
