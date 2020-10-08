39772224e5c1bd2c9b9a99a375a4cf12e7ed84c1
haru93 <syuesuta@yahoo.co.jp>
2020-10-08 18:52:19 +0900

    公開用にViewの一部修正

--- resources/views/review/create.blade.php
+++ resources/views/review/create.blade.php
@@ -13,28 +13,30 @@
     </div>
 @endif
 
-<div class="row justify-content-center container">
-    <div class="col-md-10">
-      <form method='POST' action="{{ route('review.store') }}" enctype="multipart/form-data">
-        @csrf
-        <div class="card">
-            <div class="card-body">
-              <div class="form-group">
-                <label>タイトル</label>
-                <input type='text' class='form-control' name='title' placeholder='タイトルを入力'>
+<div class="container">
+  <div class="row justify-content-center">
+      <div class="col-md-10">
+        <form method='POST' action="{{ route('review.store') }}" enctype="multipart/form-data">
+          @csrf
+          <div class="card">
+              <div class="card-body">
+                <div class="form-group">
+                  <label>タイトル</label>
+                  <input type='text' class='form-control' name='title' placeholder='タイトルを入力'>
+                </div>
+                <div class="form-group">
+                <label>レビュー本文</label>
+                  <textarea class='description form-control' name='body' placeholder='本文を入力'></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="file1">画像</label>
+                  <input type="file" id="file1" name='image' class="form-control-file">
+                </div>
+                <input type='submit' class='btn btn-primary' value='登録'>
               </div>
-              <div class="form-group">
-              <label>レビュー本文</label>
-                <textarea class='description form-control' name='body' placeholder='本文を入力'></textarea>
-              </div>
-              <div class="form-group">
-                <label for="file1">画像</label>
-                <input type="file" id="file1" name='image' class="form-control-file">
-              </div>
-              <input type='submit' class='btn btn-primary' value='登録'>
-            </div>
-        </div>
-      </form>
-    </div>
+          </div>
+        </form>
+      </div>
+  </div>
 </div>
 @endsection
